9240456c3e79bbdd1f9cc5e22e475652a077a71b
Stan Devs <atrox.cod@gmail.com>
2022-09-27 22:44:33 +0200

    Refacto input

--- src/primary/components/Input/Input.tsx
+++ src/primary/components/Input/Input.tsx
@@ -1,12 +1,29 @@
-import { InputHTMLAttributes } from "react"
+import { debounce } from "lodash";
+import { FormEvent, InputHTMLAttributes, useCallback } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
-    debounce?: number
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  debounce?: number;
 }
 
 const Input = (props: InputProps) => {
+  const onDebouncedInput = useCallback(
+    debounce((e: FormEvent<HTMLInputElement>) => {
+      if (props.onInput) props.onInput(e);
+    }, props.debounce),
+    [props.debounce, props.onInput]
+  );
 
-    return <input {...props} />
-}
+  const onInput = useCallback(
+    (e: FormEvent<HTMLInputElement>) => {
+      if (props.onInput) {
+        if (props.debounce) onDebouncedInput(e);
+        else props.onInput(e);
+      }
+    },
+    [props.onInput, props.debounce, onDebouncedInput]
+  );
 
-export default Input
+  return <input {...props} onInput={onInput} />;
+};
+
+export default Input;
--- src/primary/pages/home/Home.tsx
+++ src/primary/pages/home/Home.tsx
@@ -1,41 +1,28 @@
-import { debounce } from "lodash";
-import { FormEvent, useCallback, useState } from "react";
+import { FormEvent, useState } from "react";
 import Profile from "../../../domain/Profile/Profile";
-import ProfileRepository from "../../../secondary/Profile/ProfileRepository";
 import Input from "../../components/Input/Input";
 import useDependency from "../../hooks/useDependency";
 
 const Home = () => {
   const { profileRepository } = useDependency();
-  const [search, setSearch] = useState<string>("");
   const [profiles, setProfiles] = useState<Profile[]>([]);
 
-  const debounceFn = useCallback(
-    debounce((_search: string) => {
-      profileRepository.get(_search).then((resp) => {
-        setProfiles(resp);
-      });
-    }, 500),
-    []
-  );
-
   const handleSearch = (e: FormEvent<HTMLInputElement>) => {
-    const value = (e.target as HTMLInputElement).value;
-    setSearch(value);
-    debounceFn(value);
+    const search = (e.target as HTMLInputElement).value;
+
+    if (search.length > 2) {
+      profileRepository.get(search).then((resp) => {
+        setProfiles(resp ?? []);
+      });
+    }
   };
 
   return (
     <>
-      <Input
-        type="search"
-        value={search}
-        onInput={handleSearch}
-        debounce={1000}
-      />
+      <Input type="search" onInput={handleSearch} debounce={500} />
       <ul>
         {profiles.map((profile) => (
-          <li>{profile.login}</li>
+          <li key={profile.id}>{profile.login}</li>
         ))}
       </ul>
     </>
